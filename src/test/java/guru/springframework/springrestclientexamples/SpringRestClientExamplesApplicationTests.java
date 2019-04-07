825c80c1818f14d4f6f64e2f041c43384c4487f2
Bora Guven <guvenbe@gmail.com>
2019-04-07 01:20:14 -0400

    Iadded thymeleaf templates

--- src/test/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
+++ src/test/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package guru.springframework.springrestclientexamples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringRestClientExamplesApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
